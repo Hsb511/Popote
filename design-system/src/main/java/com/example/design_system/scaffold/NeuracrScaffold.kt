f61df6ecb75d5c7c23021d2b90419fa424ef8270
Hsb511 <hugo-selle@hotmail.fr>
2023-04-09 18:46:27 +0200

    [UPD] Improving TopBar

--- design-system/src/main/java/com/example/design_system/scaffold/NeuracrScaffold.kt
+++ design-system/src/main/java/com/example/design_system/scaffold/NeuracrScaffold.kt
@@ -16,28 +16,36 @@ import com.example.design_system.theming.NeuracrTheme
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NeuracrScaffold(
-    navItemProperties: List<NavItemProperty>,
-    content: @Composable (PaddingValues) -> Unit
+	navItemProperties: List<NavItemProperty>,
+	navigateUp: () -> Unit,
+	content: @Composable (PaddingValues) -> Unit
 ) {
-    Scaffold(
-        content = content,
-        topBar = { TopBar() },
-        bottomBar = { BottomBar(navItemProperties) },
-        floatingActionButton = { FloatingActionButton() },
-        modifier = Modifier.fillMaxSize()
-    )
+	Scaffold(
+		content = content,
+		topBar = {
+			TopBar(
+				isHome = navItemProperties.firstOrNull { it.title == "Home" }?.isSelected ?: false,
+				navigateUp = navigateUp,
+				openMenu = {},
+			)
+		},
+		bottomBar = { BottomBar(navItemProperties) },
+		floatingActionButton = { FloatingActionButton() },
+		modifier = Modifier.fillMaxSize()
+	)
 }
 
 @Composable
 @Preview(showSystemUi = true)
 fun NeuracrScaffoldPreview() {
-    NeuracrTheme {
-        NeuracrScaffold(
-            listOf(
-                NavItemProperty("Home", Icons.Filled.Home, true) {},
-                NavItemProperty("Search", Icons.Filled.Search, false) {},
-                NavItemProperty("About", Icons.Filled.Info, false) {},
-            ),
-        ) {}
-    }
+	NeuracrTheme {
+		NeuracrScaffold(
+			listOf(
+				NavItemProperty("Home", Icons.Filled.Home, true) {},
+				NavItemProperty("Search", Icons.Filled.Search, false) {},
+				NavItemProperty("About", Icons.Filled.Info, false) {},
+			),
+			{},
+		) {}
+	}
 }
