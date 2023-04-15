f5c49498d55f74272364514ecb8f6850cc455171
Hugo Selle <hugo.selle@radiofrance.com>
2023-04-15 08:34:24 +0200

    [UPD] Improving DS : fonts & colors

--- design-system/src/main/java/com/example/design_system/theming/NeuracrTypography.kt
+++ design-system/src/main/java/com/example/design_system/theming/NeuracrTypography.kt
@@ -2,50 +2,58 @@ package com.example.design_system.theming
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
-import com.example.design_system.R
+
+val googleFontProvider = GoogleFont.Provider(
+	providerAuthority = "com.google.android.gms.fonts",
+	providerPackage = "com.google.android.gms",
+	certificates = com.example.design_system.R.array.com_google_android_gms_fonts_certs
+)
+
+private fun neuracrFontFamily() = FontFamily(
+	Font(GoogleFont("Alice", true), googleFontProvider, titleFontWeight()),
+	Font(GoogleFont("Roboto", true), googleFontProvider, boldFontWeight()),
+	Font(GoogleFont("Roboto", true), googleFontProvider, bodyFontWeight()),
+)
+
 
 internal val neuracrTypography = Typography(
-    displaySmall = TextStyle(
-        //fontFamily = neuracrFontFamily(),
-        fontWeight = titleFontWeight(),
-        fontSize = 36.sp,
-    ),
-    headlineSmall = TextStyle(
-        //fontFamily = neuracrFontFamily(),
-        fontWeight = titleFontWeight(),
-        fontSize = 24.sp,
-    ),
-    titleLarge = TextStyle(
-        //fontFamily = neuracrFontFamily(),
-        fontWeight = titleFontWeight(),
-        fontSize = 22.sp,
-    ),
-    bodyLarge = TextStyle(
-        //fontFamily = neuracrFontFamily(),
-        fontWeight = bodyFontWeight(),
-        fontSize = 16.sp,
-    ),
-    labelLarge = TextStyle(
-        //fontFamily = neuracrFontFamily(),
-        fontWeight = boldFontWeight(),
-        fontSize = 14.sp,
-    ),
+	displaySmall = TextStyle(
+		fontFamily = neuracrFontFamily(),
+		fontWeight = titleFontWeight(),
+		fontSize = 32.sp,
+	),
+	headlineSmall = TextStyle(
+		fontFamily = neuracrFontFamily(),
+		fontWeight = titleFontWeight(),
+		fontSize = 26.sp,
+	),
+	titleLarge = TextStyle(
+		fontFamily = neuracrFontFamily(),
+		fontWeight = titleFontWeight(),
+		fontSize = 22.sp,
+	),
+	titleSmall = TextStyle(
+		fontFamily = neuracrFontFamily(),
+		fontWeight = boldFontWeight(),
+		fontSize = 18.sp,
+	),
+	bodyLarge = TextStyle(
+		fontFamily = neuracrFontFamily(),
+		fontWeight = boldFontWeight(),
+		fontSize = 16.sp,
+	),
+	labelLarge = TextStyle(
+		fontFamily = neuracrFontFamily(),
+		fontWeight = bodyFontWeight(),
+		fontSize = 14.sp,
+	),
 )
 
 private fun bodyFontWeight() = FontWeight.Light
 private fun boldFontWeight() = FontWeight.Medium
-private fun titleFontWeight() = FontWeight.ExtraBold
-
-//private val robotoLight = Font(R.font.roboto_light, bodyFontWeight())
-//private val robotoMedium = Font(R.font.roboto_medium, boldFontWeight())
-private val orelega1 = Font(R.font.orelega_one, FontWeight.Bold)
-private val orelega2 = Font(R.font.orelega_one, FontWeight.Medium)
-private val orelega3 = Font(R.font.orelega_one, FontWeight.Normal)
-private val orelega4 = Font(R.font.orelega_one, FontWeight.Light)
-private val orelega5 = Font(R.font.orelega_one, FontWeight.ExtraBold)
-
-private fun neuracrFontFamily() = FontFamily(orelega1, orelega2, orelega3, orelega4, orelega5) //robotoLight, robotoMedium) // orelegaOne))
+private fun titleFontWeight() = FontWeight.Black
