345c696c563b7f46335f94ec7e820f1c1c2b3ad3
Hsb511 <hugo-selle@hotmail.fr>
2023-04-16 16:12:59 +0200

    [REF] Cleaning parsers

---
+++ data/src/main/java/com/example/data/parsers/RecipeParser.kt
@@ -0,0 +1,26 @@
+package com.example.data.parsers
+
+import com.example.data.models.RecipeDataModel
+import org.jsoup.select.Elements
+import javax.inject.Inject
+
+class RecipeParser @Inject constructor() {
+	fun toRecipeDataModel(recipeId: String, rawRecipe: Elements) = RecipeDataModel(
+		href = recipeId,
+		imgSrc = rawRecipe.select("img").attr("src"),
+		title = rawRecipe.select("h1").text(),
+		subTitle = rawRecipe.select("p").first()!!.text(),
+		instructionTitle = rawRecipe
+			.select("section.recipe__instructions")
+			.html()
+			.split("<h2>")
+			.first()
+			.split("<br>")
+			.first(),
+		lastTitle = rawRecipe
+			.select("section.recipe__instructions")
+			.html()
+			.split("</div>")
+			.last(),
+	)
+}
