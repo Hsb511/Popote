c596d44ddf3538b4481a1da284703d6b2967a890
Hugo Selle <hugo.selle@radiofrance.com>
2023-05-07 19:14:20 +0200

    [UPD] Traduction des tags en français

--- data/src/test/java/com/example/data/mappers/SummarizedRecipeMapperTest.kt
+++ data/src/test/java/com/example/data/mappers/SummarizedRecipeMapperTest.kt
@@ -1,11 +1,6 @@
 package com.team23.data.mappers
 
-import com.team23.data.fixtures.getFirstLocalDate
-import com.team23.data.fixtures.getFirstSummarizedRecipeDomainModel
-import com.team23.data.fixtures.getFirstSummarizedRecipeFromRawHtml
-import com.team23.data.fixtures.getSecondLocalDate
-import com.team23.data.fixtures.getSecondSummarizedRecipeDomainModel
-import com.team23.data.fixtures.getSecondSummarizedRecipeFromRawHtml
+import com.team23.data.fixtures.*
 import com.team23.data.models.SummarizedRecipeDataModel
 import com.team23.domain.models.RecipeDomainModel
 import io.mockk.every
@@ -14,40 +9,41 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class SummarizedRecipeMapperTest {
-    private val dateMapper: DateMapper = mockk()
-    private val summarizedRecipeMapper = SummarizedRecipeMapper(dateMapper)
+	private val dateMapper: DateMapper = mockk()
+	private val languageMapper: LanguageMapper = mockk()
+	private val summarizedRecipeMapper = SummarizedRecipeMapper(dateMapper, languageMapper)
 
-    @Test
-    fun `Given valid data models, When toSummarizedRecipeDomainModels, Then return valid corresponding domain models`() {
-        // Given
-        val validDataModels = listOf(
-            getFirstSummarizedRecipeFromRawHtml(),
-            getSecondSummarizedRecipeFromRawHtml(),
-        )
-        every { dateMapper.toLocalDate("2022/12/19") } returns getFirstLocalDate()
-        every { dateMapper.toLocalDate("2022/12/04") } returns getSecondLocalDate()
+	@Test
+	fun `Given valid data models, When toSummarizedRecipeDomainModels, Then return valid corresponding domain models`() {
+		// Given
+		val validDataModels = listOf(
+			getFirstSummarizedRecipeFromRawHtml(),
+			getSecondSummarizedRecipeFromRawHtml(),
+		)
+		every { dateMapper.toLocalDate("2022/12/19") } returns getFirstLocalDate()
+		every { dateMapper.toLocalDate("2022/12/04") } returns getSecondLocalDate()
 
-        // When
-        val actualDomainModels = summarizedRecipeMapper.toSummarizedRecipeDomainModels(validDataModels)
-        val expectedDomainModels = listOf(
-            getFirstSummarizedRecipeDomainModel(),
-            getSecondSummarizedRecipeDomainModel()
-        )
+		// When
+		val actualDomainModels = summarizedRecipeMapper.toSummarizedRecipeDomainModels(validDataModels)
+		val expectedDomainModels = listOf(
+			getFirstSummarizedRecipeDomainModel(),
+			getSecondSummarizedRecipeDomainModel()
+		)
 
-        // Then
-        assertEquals(expectedDomainModels, actualDomainModels)
-    }
+		// Then
+		assertEquals(expectedDomainModels, actualDomainModels)
+	}
 
-    @Test
-    fun `Given empty data models, When toSummarizedRecipeDomainModels, Then return empty domain models`() {
-        // Given
-        val emptyDataModels = emptyList<SummarizedRecipeDataModel>()
+	@Test
+	fun `Given empty data models, When toSummarizedRecipeDomainModels, Then return empty domain models`() {
+		// Given
+		val emptyDataModels = emptyList<SummarizedRecipeDataModel>()
 
-        // When
-        val actualDomainModels = summarizedRecipeMapper.toSummarizedRecipeDomainModels(emptyDataModels)
-        val expectedDomainModels = emptyList<RecipeDomainModel.Summarized>()
+		// When
+		val actualDomainModels = summarizedRecipeMapper.toSummarizedRecipeDomainModels(emptyDataModels)
+		val expectedDomainModels = emptyList<RecipeDomainModel.Summarized>()
 
-        // Then
-        assertEquals(expectedDomainModels, actualDomainModels)
-    }
+		// Then
+		assertEquals(expectedDomainModels, actualDomainModels)
+	}
 }
--- data/src/test/java/com/example/data/repositories/RecipeRepositoryImplTest.kt
+++ data/src/test/java/com/example/data/repositories/RecipeRepositoryImplTest.kt
@@ -1,9 +1,12 @@
-package com.team23.data.repositories
+package com.example.data.repositories
 
-import com.team23.data.daos.SummarizedRecipeDao
+import com.team23.data.daos.*
 import com.team23.data.datasources.NeuracrWebsiteDataSource
-import com.team23.data.parsers.SummarizedRecipeParser
+import com.team23.data.mappers.FullRecipeMapper
 import com.team23.data.mappers.SummarizedRecipeMapper
+import com.team23.data.parsers.FullRecipeParser
+import com.team23.data.parsers.SummarizedRecipeParser
+import com.team23.data.repositories.RecipeRepositoryImpl
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
@@ -11,19 +14,35 @@ import org.junit.Test
 
 @ExperimentalCoroutinesApi
 class RecipeRepositoryImplTest {
-    private val summarizedRecipeDao: SummarizedRecipeDao = mockk()
-    private val neuracrWebsiteDataSource: NeuracrWebsiteDataSource = mockk()
-    private val summarizedRecipeParser: SummarizedRecipeParser = mockk()
-    private val summarizedRecipeMapper: SummarizedRecipeMapper = mockk()
+	private val summarizedRecipeDao: SummarizedRecipeDao = mockk()
+	private val recipeDao: RecipeDao = mockk()
+	private val tagDao: TagDao = mockk()
+	private val ingredientDao: IngredientDao = mockk()
+	private val instructionDao: InstructionDao = mockk()
+	private val neuracrWebsiteDataSource: NeuracrWebsiteDataSource = mockk()
+	private val summarizedRecipeParser: SummarizedRecipeParser = mockk()
+	private val summarizedRecipeMapper: SummarizedRecipeMapper = mockk()
+	private val fullRecipeParser: FullRecipeParser = mockk()
+	private val fullRecipeMapper: FullRecipeMapper = mockk()
 
-    private val recipeRepositoryImpl = RecipeRepositoryImpl(
-        summarizedRecipeDao, neuracrWebsiteDataSource, summarizedRecipeParser, summarizedRecipeMapper
-    )
+	private val recipeRepositoryImpl = RecipeRepositoryImpl(
+		summarizedRecipeDao,
+		recipeDao,
+		tagDao,
+		ingredientDao,
+		instructionDao,
+		neuracrWebsiteDataSource,
+		summarizedRecipeParser,
+		fullRecipeParser,
+		summarizedRecipeMapper,
+		fullRecipeMapper,
+	)
 
-    @Test
-    fun `Given repo return recipe data models, When getAllSummarizedRecipes is called, Then return valid domain models`() = runTest {
-        // Given
-        //every { recipeDao.getAll() } returns
-
-    }
+	@Test
+	fun `Given repo return recipe data models, When getAllSummarizedRecipes is called, Then return valid domain models`() =
+		runTest {
+			// Given
+			//every { recipeDao.getAll() } returns
+			recipeRepositoryImpl
+		}
 }
