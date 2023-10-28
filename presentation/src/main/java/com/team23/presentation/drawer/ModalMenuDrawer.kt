23c94304fff0bde51069d095014232afb5399883
Hsb511 <hugo-selle@hotmail.fr>
2023-10-28 11:33:21 +0200

    [ADD] Drawer item 'change nickname'

--- presentation/src/main/java/com/team23/presentation/drawer/ModalMenuDrawer.kt
+++ presentation/src/main/java/com/team23/presentation/drawer/ModalMenuDrawer.kt
@@ -1,14 +1,20 @@
 package com.team23.presentation.drawer
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.team23.design_system.theming.NeuracrTheme
 import com.team23.presentation.drawer.models.DrawerUiModel
 import com.team23.presentation.drawer.views.DrawerFooter
 import com.team23.presentation.drawer.views.DrawerHeader
+import com.team23.presentation.drawer.views.NicknameDrawerItem
 
 @Composable
 fun ModalMenuDrawer(drawerUiModel: DrawerUiModel, modifier: Modifier = Modifier, content: @Composable () -> Unit) {
@@ -18,17 +24,17 @@ fun ModalMenuDrawer(drawerUiModel: DrawerUiModel, modifier: Modifier = Modifier,
 		gesturesEnabled = false,
 		drawerContent = {
 			ModalDrawerSheet {
-				Column(modifier = Modifier.fillMaxSize()) {
-					DrawerHeader(versionName = drawerUiModel.versionName)
-
-					Spacer(modifier = Modifier.weight(1f))
-					DrawerFooter()
-				}
+				Spacer(modifier = Modifier.height(64.dp)) // TopAppBar height
+				DrawerHeader(versionName = drawerUiModel.versionName)
+				NicknameDrawerItem(nickname = drawerUiModel.nickname)
+				Spacer(modifier = Modifier.weight(1f))
+				DrawerFooter()
 			}
 		},
 		content = content,
 	)
 }
+
 
 @Composable
 @Preview(showSystemUi = true)
@@ -37,7 +43,8 @@ fun ModalMenuDrawerPreview() {
 		ModalMenuDrawer(
 			DrawerUiModel(
 				rememberDrawerState(initialValue = DrawerValue.Open),
-				versionName = "23.23.0"
+				versionName = "23.23.0",
+				nickname = "GOGO"
 			)
 		) {}
 	}
--- presentation/src/main/java/com/team23/presentation/drawer/models/DrawerUiModel.kt
+++ presentation/src/main/java/com/team23/presentation/drawer/models/DrawerUiModel.kt
@@ -1,10 +1,9 @@
 package com.team23.presentation.drawer.models
 
 import androidx.compose.material3.DrawerState
-import androidx.compose.material3.ExperimentalMaterial3Api
 
-@OptIn(ExperimentalMaterial3Api::class)
 data class DrawerUiModel(
 	val drawerState: DrawerState,
 	val versionName: String,
+	val nickname: String?,
 )
