878f831735b03f708c00d57d9719659fd6015b2c
Hsb511 <hugo-selle@hotmail.fr>
2023-12-28 17:45:22 +0100

    [ADD] .gitignore and delete useless files

--- presentation/src/androidMain/kotlin/com/team23/neuracrsrecipes/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package com.team23.neuracrsrecipes
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
