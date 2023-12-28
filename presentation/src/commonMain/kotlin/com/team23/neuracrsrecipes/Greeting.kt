6b11a7c583e2f04c5765b84346dcc966a7f6e12e
Hsb511 <hugo-selle@hotmail.fr>
2023-12-28 17:45:22 +0100

    [ADD] .gitignore and delete useless files

--- presentation/src/commonMain/kotlin/com/team23/neuracrsrecipes/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.team23.neuracrsrecipes
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- presentation/src/commonMain/kotlin/com/team23/neuracrsrecipes/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.team23.neuracrsrecipes
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
