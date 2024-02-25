3ceb7a243c579dc1798975651c06c5acb8077769
Hsb511 <hugo-selle@hotmail.fr>
2024-02-25 16:41:19 +0100

    [ADD] User Repository @ Dao

--- data/src/commonMain/kotlin/com/team23/data/datasource/NeuracrLocalDataSource.kt
+++ data/src/commonMain/kotlin/com/team23/data/datasource/NeuracrLocalDataSource.kt
@@ -2,6 +2,7 @@ package com.team23.data.datasource
 
 import com.team23.data.dao.FavoriteDao
 import com.team23.data.dao.SummarizedRecipeDao
+import com.team23.data.dao.UserDao
 import data.AppDatabase
 
 
@@ -11,4 +12,5 @@ internal class NeuracrLocalDataSource(databaseDriverFactory: DatabaseDriverFacto
 
     internal val summarizedRecipeDao = SummarizedRecipeDao(dbQueries)
     internal val favoriteDao = FavoriteDao(dbQueries)
+    internal val userDao = UserDao(dbQueries)
 }
