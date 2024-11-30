2d31bbc54168e941b4441411060f1fa9c704453a
Hsb511 <hugo-selle@hotmail.fr>
2024-11-30 14:41:31 +0100

    [iOS] #1 first adaptations: actual implementation for mapper and extensions for iOS + iOS config files

--- presentation/src/iosMain/kotlin/com/team23/neuracrsrecipes/PresentationModule.ios.kt
+++ presentation/src/iosMain/kotlin/com/team23/neuracrsrecipes/PresentationModule.ios.kt
@@ -1,7 +1,10 @@
 package com.team23.neuracrsrecipes
 
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.module
+import com.team23.neuracrsrecipes.handler.UiActionHandler
 
-internal actual fun platformModule(): Module {
-    TODO("Not yet implemented")
+internal actual fun platformModule(): Module = module {
+    singleOf(::UiActionHandler)
 }
