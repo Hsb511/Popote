3ccd4047e5a1905629eecaf9a33fc1e301d43780
Hsb511 <hugo-selle@hotmail.fr>
2025-02-22 17:15:52 +0100

    [ADD] Display ingredients list on top of the recipe image when scrolling passed ingredient list

--- view/src/commonMain/kotlin/com/team23/view/widget/recipe/RecipeContentData.kt
+++ view/src/commonMain/kotlin/com/team23/view/widget/recipe/RecipeContentData.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -29,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.positionInRoot
@@ -41,6 +44,7 @@ import com.team23.view.Res
 import com.team23.view.ds.button.ButtonLike
 import com.team23.view.ds.button.ButtonLocalPhone
 import com.team23.view.ds.image.NeuracrImage
+import com.team23.view.extension.getCurrentScreenWidth
 import com.team23.view.extension.getImageMaxHeight
 import com.team23.view.extension.horizontalGutterPadding
 import com.team23.view.mapper.FavoriteUiMapper
@@ -75,9 +79,13 @@ fun RecipeContentData(
     ) { padding ->
         val density = LocalDensity.current
         val horizontalPadding = horizontalGutterPadding.value
-        var yPosition by remember { mutableStateOf(0f) }
-        val coeff = (1 - yPosition / (4 * horizontalPadding)).coerceIn(0f, 1f)
+        val imageHeight = getCurrentScreenWidth() * 3f / 4 - 64.dp
+        var yPositionImage by remember { mutableStateOf(0f) }
+        var yPositionBottomIngredients by remember { mutableStateOf(0f) }
+        var yPositionTopInstructions by remember { mutableStateOf(0f) }
+        val coeff = (1 - yPositionImage / (4 * horizontalPadding)).coerceIn(0f, 1f)
         val spaceToAddInPx = with(LocalDensity.current) { horizontalPadding.dp.toPx() } * 2
+        val headerIngredientFraction = (yPositionBottomIngredients / (yPositionBottomIngredients - yPositionTopInstructions)).coerceIn(0f, 1f)
 
         Box(modifier = modifier.fillMaxSize()) {
             Column(
@@ -122,7 +130,7 @@ fun RecipeContentData(
                         .padding(vertical = 8.dp)
                         .fillMaxWidth()
                         .onGloballyPositioned { layoutCoordinates ->
-                            yPosition = layoutCoordinates.positionInRoot().y
+                            yPositionImage = layoutCoordinates.positionInRoot().y
                         }
                         .graphicsLayer {
                             scaleX = (size.width + spaceToAddInPx * coeff) / size.width
@@ -137,13 +145,18 @@ fun RecipeContentData(
                     modifier = Modifier.padding(top = 16.dp, bottom = 4.dp),
                 )
                 RecipeIngredientsWidget(
-                    IngredientsUiModel.FromRecipeScreen(
+                    ingredientsUiModel = IngredientsUiModel.FromRecipeScreen(
                         ingredients = recipeUiModel.ingredients,
                         currentServingsAmount = currentServingsAmount,
                         onValueChanged = onValueChanged,
                         onAddOneServing = onAddOneServing,
                         onSubtractOneServing = onSubtractOneServing,
                     ),
+                    modifier = Modifier.onGloballyPositioned { layoutCoordinates ->
+                        yPositionBottomIngredients = layoutCoordinates.positionInRoot().y +
+                            layoutCoordinates.size.height -
+                            with(density) { (imageHeight + 24.dp).toPx() }
+                    }
                 )
                 HorizontalDivider(modifier = Modifier.padding(top = 8.dp))
                 Text(
@@ -153,27 +166,63 @@ fun RecipeContentData(
                 Text(
                     text = stringResource(Res.string.recipe_instructions_title),
                     style = MaterialTheme.typography.headlineSmall,
-                    modifier = Modifier.padding(top = 4.dp, bottom = 8.dp),
+                    modifier = Modifier
+                        .padding(top = 4.dp, bottom = 8.dp)
+                        .onGloballyPositioned { layoutCoordinates ->
+                            yPositionTopInstructions = layoutCoordinates.positionInRoot().y -
+                                with(density) { (imageHeight + 24.dp).toPx() }
+                        }
                 )
-                RecipeInstructionsWidget(InstructionsUiModel.FromRecipeScreen(recipeUiModel.instructions))
+                RecipeInstructionsWidget(
+                    instructions = InstructionsUiModel.FromRecipeScreen(recipeUiModel.instructions),
+                )
                 Text(
                     text = recipeUiModel.conclusion,
                     style = MaterialTheme.typography.bodyMedium,
                     modifier = Modifier.padding(top = 16.dp),
                 )
             }
-            if (yPosition <= 0f) {
-                RecipeImage(
-                    recipeUiModel, onFavoriteClick, onLocalPhoneClick,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = horizontalPadding.dp)
-                        .graphicsLayer {
-                            scaleX = (size.width + spaceToAddInPx) / size.width
-                            scaleY = (size.height + spaceToAddInPx) / size.height
-                            translationY = -with(density) { horizontalPadding.dp.toPx() } * coeff
-                        }
-                )
+            Box {
+                if (yPositionImage <= 0f) {
+                    RecipeImage(
+                        recipeUiModel, onFavoriteClick, onLocalPhoneClick,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(MaterialTheme.colorScheme.surface.copy(alpha = 0.5f))
+                            .padding(horizontal = horizontalPadding.dp)
+                            .graphicsLayer {
+                                scaleX = (size.width + spaceToAddInPx) / size.width
+                                scaleY = (size.height + spaceToAddInPx) / size.height
+                                translationY =
+                                    -with(density) { horizontalPadding.dp.toPx() } * coeff
+                            }
+                    )
+                }
+                if (yPositionBottomIngredients <= 0f) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(imageHeight + 16.dp)
+                            .background(
+                                color = MaterialTheme.colorScheme.surface.copy(alpha = headerIngredientFraction * 0.5f),
+                                shape = RoundedCornerShape(16.dp),
+                            )
+                    )
+                    RecipeIngredientsColumn(
+                        ingredientsUiModel = IngredientsUiModel.FromRecipeScreen(
+                            ingredients = recipeUiModel.ingredients,
+                            currentServingsAmount = currentServingsAmount,
+                            onValueChanged = onValueChanged,
+                            onAddOneServing = onAddOneServing,
+                            onSubtractOneServing = onSubtractOneServing,
+                        ),
+                        modifier = Modifier
+                            .padding(top = 64.dp)
+                            .height(imageHeight - 48.dp)
+                            .alpha(headerIngredientFraction)
+                            .verticalScroll(rememberScrollState())
+                    )
+                }
             }
         }
     }
