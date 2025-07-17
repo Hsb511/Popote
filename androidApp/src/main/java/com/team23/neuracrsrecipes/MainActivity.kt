322bbb8da43acaa39dbe3259d3066225fd07ef00
Hugo Selle <hugo.selle@quickline.net>
2025-07-17 14:30:00 +0200

    [FIX] Status bar color

--- androidApp/src/main/java/com/team23/neuracrsrecipes/MainActivity.kt
+++ androidApp/src/main/java/com/team23/neuracrsrecipes/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
 import com.team23.view.MainContainer
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightStatusBars = false
         setContent {
             MainContainer()
         }
