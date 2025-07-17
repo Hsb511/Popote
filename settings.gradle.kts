9cba1a9ba0dbbc48296169613a778a7cc5a12b3b
Hugo Selle <hugo.selle@quickline.net>
2025-07-17 11:32:32 +0200

    [UPD] Adaptation for iOS

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,13 @@ dependencyResolutionManagement {
 
 rootProject.name = "PopoteV2"
 include(":androidApp")
+include(":iosApp")
 include(":presentation")
 include(":view")
 include(":domain")
 include(":data")
+
+project(":domain").projectDir = file("domain")
+project(":data").projectDir = file("data")
+project(":presentation").projectDir = file("presentation")
+project(":view").projectDir = file("view")
