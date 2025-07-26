3109515273158af3941423b4a12a9f33ff200027
Hsb511 <hugo-selle@hotmail.fr>
2025-07-26 11:03:41 +0200

    [FIX] Use toUri() + fix crash when opening add screen after image access has been lost

--- presentation/src/commonMain/kotlin/com/team23/neuracrsrecipes/extension/ImageExtensions.kt
+++ presentation/src/commonMain/kotlin/com/team23/neuracrsrecipes/extension/ImageExtensions.kt
@@ -4,4 +4,4 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.ImageBitmap
 
 @Composable
-expect fun getImageBitmapFromUri(uri: String): ImageBitmap
+expect fun getImageBitmapFromUri(uri: String): ImageBitmap?
