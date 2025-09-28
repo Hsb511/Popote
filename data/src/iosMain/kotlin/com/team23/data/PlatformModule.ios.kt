8fdc407c09f9da82aad9f2ec3a641b49bd6460af
Hugo Selle <hugo.selle@quickline.net>
2025-09-28 15:02:12 +0200

    [FIX] Add HttpClientEngine for iOS

--- data/src/iosMain/kotlin/com/team23/data/PlatformModule.ios.kt
+++ data/src/iosMain/kotlin/com/team23/data/PlatformModule.ios.kt
@@ -1,10 +1,13 @@
 package com.team23.data
 
 import com.team23.data.datasource.DatabaseDriverFactory
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.darwin.Darwin
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 
 actual fun platformModule() = module {
     singleOf(::DatabaseDriverFactory)
+    factory<HttpClientEngine> { Darwin.create() }
 }
