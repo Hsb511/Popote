ac2284382c899d749835a1b2deabe0ba66b4dd25
Hsb511 <hugo-selle@hotmail.fr>
2026-06-19 16:39:46 +0200

    [UPD] Select image source dialog

--- view/src/commonMain/kotlin/com/team23/view/widget/add/ImageSourceOptionsDialog.kt
+++ view/src/commonMain/kotlin/com/team23/view/widget/add/ImageSourceOptionsDialog.kt
@@ -1,6 +1,5 @@
 package com.team23.view.widget.add
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -11,29 +10,24 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import com.team23.view.Res
+import com.team23.view.add_recipe_select_image_source_camera
+import com.team23.view.add_recipe_select_image_source_gallery
+import com.team23.view.add_recipe_select_image_source_title
 import com.team23.view.ic_camera
 import com.team23.view.ic_gallery
-import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun ImageSourceOptionDialog(
@@ -46,36 +40,51 @@ fun ImageSourceOptionDialog(
         properties = DialogProperties(dismissOnBackPress = true, dismissOnClickOutside = true)
     ) {
         Column(
-            modifier = Modifier.fillMaxWidth().background(MaterialTheme.colorScheme.surface)
-                .padding(20.dp), horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = MaterialTheme.colorScheme.surface,
+                    shape = MaterialTheme.shapes.large,
+                )
+                .padding(20.dp),
         ) {
             Text(
-                text = "Select an Image Source",
+                text = stringResource(Res.string.add_recipe_select_image_source_title),
                 color = MaterialTheme.colorScheme.onSurface,
                 fontWeight = FontWeight.Bold
             )
             Spacer(modifier = Modifier.height(10.dp))
             Row(
-                modifier = Modifier.fillMaxWidth().padding(vertical = 15.dp).clickable {
-                    onCameraRequest.invoke()
-                },
                 verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(10.dp)
+                horizontalArrangement = Arrangement.spacedBy(10.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 15.dp)
+                    .clickable(enabled = false) {
+                        onCameraRequest.invoke()
+                    },
             ) {
                 Icon(
-                    tint = MaterialTheme.colorScheme.onSurface,
+                    tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f),
                     modifier = Modifier.size(25.dp),
                     painter = painterResource(Res.drawable.ic_camera),
                     contentDescription = null
                 )
-                Text(text = "Camera", color = MaterialTheme.colorScheme.onSurface)
+                Text(
+                    text = stringResource(Res.string.add_recipe_select_image_source_camera),
+                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f),
+                )
             }
             Row(
-                modifier = Modifier.fillMaxWidth().padding(vertical = 15.dp).clickable {
-                    onGalleryRequest.invoke()
-                },
                 verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(10.dp)
+                horizontalArrangement = Arrangement.spacedBy(10.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 15.dp)
+                    .clickable {
+                        onGalleryRequest.invoke()
+                    },
             ) {
                 Icon(
                     tint = MaterialTheme.colorScheme.onSurface,
@@ -83,97 +92,10 @@ fun ImageSourceOptionDialog(
                     painter = painterResource(Res.drawable.ic_gallery),
                     contentDescription = null
                 )
-                Text(text = "Gallery", color = MaterialTheme.colorScheme.onSurface)
-            }
-        }
-    }
-}
-
-@Composable
-fun AlertMessageDialog(
-    title: String,
-    message: String? = null,
-    resource: DrawableResource? = null,
-    positiveButtonText: String? = null,
-    negativeButtonText: String? = null,
-    onPositiveClick: () -> Unit = {},
-    onNegativeClick: () -> Unit = {},
-) {
-
-    Dialog(
-        onDismissRequest = {}, properties = DialogProperties(
-            dismissOnBackPress = false,
-            dismissOnClickOutside = false,
-        )
-    ) {
-        Surface(
-            modifier = Modifier.fillMaxWidth().wrapContentHeight(),
-            shape = RoundedCornerShape(size = 12.dp)
-        ) {
-            Column(
-                modifier = Modifier.background(MaterialTheme.colorScheme.secondaryContainer)
-                    .padding(all = 16.dp),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                resource?.let {
-                    Image(
-                        modifier = Modifier.size(100.dp),
-                        painter = painterResource(it),
-                        contentDescription = null
-                    )
-                }
-                Spacer(modifier = Modifier.height(20.dp))
                 Text(
-                    text = title,
-                    fontSize = MaterialTheme.typography.displayLarge.fontSize,
-                    fontWeight = FontWeight.Medium,
-                    color = MaterialTheme.colorScheme.onBackground,
-                    textAlign = TextAlign.Center
+                    text = stringResource(Res.string.add_recipe_select_image_source_gallery),
+                    color = MaterialTheme.colorScheme.onSurface,
                 )
-                Spacer(modifier = Modifier.height(10.dp))
-                message?.let {
-                    Text(
-                        text = it,
-                        fontSize = MaterialTheme.typography.titleSmall.fontSize,
-                        fontWeight = FontWeight.Medium,
-                        color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.5f),
-                        textAlign = TextAlign.Center
-                    )
-                }
-                Spacer(modifier = Modifier.height(15.dp))
-                Row(
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.fillMaxWidth().padding(end = 16.dp, start = 16.dp)
-                ) {
-                    negativeButtonText?.let {
-                        Button(
-                            modifier = Modifier.weight(1f), onClick = {
-                                onNegativeClick()
-                            }, colors = ButtonDefaults.buttonColors(
-                                contentColor = Color.White,
-                                containerColor = MaterialTheme.colorScheme.primary
-                            )
-                        ) {
-                            Text(text = it, textAlign = TextAlign.Center, maxLines = 1)
-                        }
-
-                        Spacer(modifier = Modifier.width(6.dp))
-                    }
-                    positiveButtonText?.let {
-                        Button(
-                            modifier = Modifier.weight(1f), onClick = {
-                                onPositiveClick()
-                            }, colors = ButtonDefaults.buttonColors(
-                                contentColor = Color.White,
-                                containerColor = MaterialTheme.colorScheme.primary
-                            )
-                        ) {
-                            Text(text = it, textAlign = TextAlign.Center, maxLines = 1)
-                        }
-                    }
-                }
             }
         }
     }
