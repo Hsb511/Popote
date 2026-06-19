97662e53a948058ed56a12650a4e12a5083c9fde
Hsb511 <hugo-selle@hotmail.fr>
2026-06-19 22:32:49 +0200

    [UPD] Add cuisine flag favorite list

--- presentation/src/commonMain/kotlin/com/team23/neuracrsrecipes/viewmodel/FavoriteViewModel.kt
+++ presentation/src/commonMain/kotlin/com/team23/neuracrsrecipes/viewmodel/FavoriteViewModel.kt
@@ -3,10 +3,13 @@ package com.team23.neuracrsrecipes.viewmodel
 import com.team23.domain.favorite.repository.FavoriteRepository
 import com.team23.domain.preference.usecase.GetPreferenceDisplayTypeUseCase
 import com.team23.domain.preference.usecase.UpdatePreferenceUseCase
+import com.team23.domain.recipe.usecase.GetFullRecipeByIdUseCase
 import com.team23.neuracrsrecipes.extension.next
 import com.team23.neuracrsrecipes.handler.SnackbarHandler
 import com.team23.neuracrsrecipes.mapper.DisplayTypeUiMapper
 import com.team23.neuracrsrecipes.mapper.SummarizedRecipeUiMapper
+import com.team23.neuracrsrecipes.mapper.TagUiMapper
+import com.team23.neuracrsrecipes.model.uimodel.SummarizedRecipeUiModel
 import com.team23.neuracrsrecipes.model.uistate.FavoriteUiState
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -15,13 +18,16 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class FavoriteViewModel(
     private val favoriteRepository: FavoriteRepository,
     private val getPreferenceDisplayTypeUseCase: GetPreferenceDisplayTypeUseCase,
     private val updatePreferenceUseCase: UpdatePreferenceUseCase,
+    private val getFullRecipeByIdUseCase: GetFullRecipeByIdUseCase,
     private val summarizedRecipeUiMapper: SummarizedRecipeUiMapper,
     private val displayTypeUiMapper: DisplayTypeUiMapper,
+    private val tagUiMapper: TagUiMapper,
     private val viewModelScope: CoroutineScope,
     private val snackbarHandler: SnackbarHandler,
 ) {
@@ -35,8 +41,10 @@ class FavoriteViewModel(
             favoriteRepository.getAllFavorites().map { recipes ->
                 recipes.map { summarizedRecipeUiMapper.toUiModel(it) }
             }.collect { favorites ->
+                loadCuisineFlags(favorites)
                 val currentDisplayType = (_uiState.value as? FavoriteUiState.Data.WithFavorites)?.displayType
                     ?: displayType
+
                 _uiState.value = if (favorites.isEmpty()) {
                     FavoriteUiState.Data.Empty
                 } else {
@@ -44,6 +52,25 @@ class FavoriteViewModel(
                         displayType = currentDisplayType,
                         favorites = favorites,
                     )
+                }
+            }
+        }
+    }
+
+    private fun loadCuisineFlags(recipes: List<SummarizedRecipeUiModel>) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val updatedRecipesWithCuisine = recipes.map { recipe ->
+                val fullRecipe = getFullRecipeByIdUseCase.invoke(recipe.id).getOrNull()
+                if (fullRecipe != null) {
+                    recipe.copy(cuisineFlag = tagUiMapper.toFlagProperty(fullRecipe.tags),)
+                } else {
+                    recipe
+                }
+            }
+            withContext(Dispatchers.Main) {
+                _uiState.value = when (val currentState = _uiState.value) {
+                    is FavoriteUiState.Data.WithFavorites -> currentState.copy(favorites = updatedRecipesWithCuisine)
+                    else -> currentState
                 }
             }
         }
