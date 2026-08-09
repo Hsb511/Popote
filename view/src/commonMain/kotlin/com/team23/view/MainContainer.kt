74884a61f79d21aeb38d48ca7c76b6177f0362e6
Hsb511 <hugo-selle@hotmail.fr>
2026-08-09 17:04:28 +0200

    [VER] v2.3.1

--- view/src/commonMain/kotlin/com/team23/view/MainContainer.kt
+++ view/src/commonMain/kotlin/com/team23/view/MainContainer.kt
@@ -62,7 +62,7 @@ fun MainContainer() {
                         isNavigationEmpty = !navigator.canPop
                     ) { padding ->
                         ModalMenuDrawer(
-                            drawerUiModel = DrawerUiModel("2.3.0"),
+                            drawerUiModel = DrawerUiModel("2.3.1"),
                             drawerState = drawerState,
                         ) {
                             Box(modifier = Modifier
